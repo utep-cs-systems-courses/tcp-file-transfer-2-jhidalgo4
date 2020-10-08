5d850919b85541a41da178a395ed3c408007bf8f
jhidalgo4 <jhidalgo4@miners.utep.edu>
2020-10-08 00:06:42 -0600

    working passing multiple files

--- file-transfer-lab/fileClient.py
+++ file-transfer-lab/fileClient.py
@@ -5,17 +5,97 @@ Created on Thur Sep 24 19:02:15 2020
 
 @author: joaquin
 """
-import socket
+#SIMPLE VERSION
+# import socket
 
-cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-cs.connect(("127.0.0.1", 50001)) # connect to my computer (client)
+# cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# cs.connect(("127.0.0.1", 50001)) # connect to my computer (client)
 
-#Read file
-with open ('fileSend.txt', 'r') as inputFile:
-    fileTrans = inputFile.read()
-    cs.send(fileTrans.encode() )
+# #Read file
+# with open ('fileSend.txt', 'r') as inputFile:
+#     fileTrans = inputFile.read()
+#     cs.send(fileTrans.encode() )
+    
+# print('end of fileClient.py')
+# cs.close()
+
+
+import socket, sys, re, os
+sys.path.append("../lib")
+import params
+sys.path.append("../framed-echo")
+import framedSock
+
+switchesVarDefaults = (
+    (('-s', '--server'), 'server', "127.0.0.1:50001"),
+    (('-d', 'debug'), "debug", False),
+    (('-?', '--usage'), "usage", False), # boolean (set if present)
+    )
+
+paramMap = params.parseParams(switchesVarDefaults)
+server, usage, debug = paramMap["server"],paramMap["usage"],paramMap["debug"]
+
+if usage:
+    params.usage()
+    
+#split/parse switchesVarDefaults
+try:
+    serverHost, serverPort = re.split(":", server)
+    serverPort = int(serverPort)
+except:
+    print('unable to parse "server" arg... exit')
+    sys.exit(1)
+serverPortAddr = (serverHost, serverPort)
+
+
+#Create Socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+if s is None:
+    print('Could not open socket... exit')
+    sys.exit(1)
+s.connect(serverPortAddr)
+
+while True:
+    #Obtain user input
+    fileToSend = input('Enter the name of file you would like to send (or exit) ')
+    fileToSend = fileToSend.strip()
+    
+    if fileToSend == 'exit':
+        print('exiting... goodbye')
+        s.sendall("exit".encode() )
+        sys.exit(0)
+    
+    #Check if file exists
+    if os.path.exists(fileToSend):
+        print('\nPath exists: ', fileToSend)
+        #send file name
+        s.sendall(fileToSend.encode() )
+            #Open file, begin to send bytes
+        with open(fileToSend, 'r') as contentPayload:
+            print('opening: ', fileToSend)
+            msg=contentPayload.read()
+            if len(msg) == 0:
+                print('empty File')
+            else:
+                print('Beginning to send: ', fileToSend)
+                #framedSock.framedSend(s, msg, debug)
+                s.sendall(msg.encode() )
+        print('From server: Status of writting message: ', s.recv(1024).decode(), '\n' )
+        
+    else:
+        print(fileToSend,' --> filePath does not exist')
+        s.sendall(fileToSend.encode() )
+        # sys.exit(1)
+        # continue
+    
+#send file name
+#framedSock.framedSend(s, bytes(fileToSend, encoding='utf-8'), debug) #hmmmmm
     
 
-print('end of fileClient.py')
-cs.close()
-    
+
+
+
+
+
+
+
--- file-transfer-lab/fileServer.py
+++ file-transfer-lab/fileServer.py
@@ -5,22 +5,97 @@ Created on Thur Sep 24 19:00:04 2020
 
 @author: joaquin
 """
-import socket
+#SIMPLE VERSION
+# import socket
 
-ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ls.bind(('',50001)) #bind the specific port to the OS
+# ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# ls.bind(('',50001)) #bind the specific port to the OS
 
-ls.listen(1) #listen mode - to 1 port
+# ls.listen(1) #listen mode - to 1 port
 
-convoS, addr = ls.accept() #this accepts connection
+# convoS, addr = ls.accept() #this accepts connection
 
-with open ('recievedFile.txt', 'w') as writeFile:
-    transWord = convoS.recv(1024).decode()
-    for l in transWord:
-        writeFile.write(l)
+# with open ('recievedFile.txt', 'w') as writeFile:
+#     transWord = convoS.recv(1024).decode()
+#     for l in transWord:
+#         writeFile.write(l)
         
-print('end of fileServer.py')
-convoS.close()
+# print('end of fileServer.py')
+# convoS.close()
+
+
+import sys, socket, os
+sys.path.append("../lib")
+import params
+sys.path.append("../framed-echo")
+import framedSock
+
+host = "127.0.0.1"
+DIR = './recieve/'
+
+switchesVarDefaults = (
+    (('-l', '--listenPort'), 'listenPort', 50001),
+    (('-d', 'debug'), "debug", False),
+    (('-?', '--usage'), "usage", False), # boolean (set if present)
+    )
+
+paramMap = params.parseParams(switchesVarDefaults)
+listenPort, usage, debug = paramMap["listenPort"],paramMap["usage"],paramMap["debug"]
+
+if usage:
+    params.usage()
+
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+bindAddr = (host,listenPort)
+lsock.bind(bindAddr)
+lsock.listen()
+print('listening on (host/port): ', bindAddr)
+
+sock,addr = lsock.accept()
+print('connection recieved from: ', addr)
+
+while True:
+    # if not os.fork():
+        #obtain fileName to write
+        #userFileName = framedSock.framedReceive(sock, debug)
+        #userFileName = userFileName.decode()
+        
+    userInput = sock.recv(1024).decode()
+    if os.path.exists(userInput):
+        print('Obtaining: ', userInput)
+        fileSize = int( os.stat(userInput).st_size )
+        print('File Size: ', fileSize)
+        
+        payload = ''
+        thisBuffSize = 0
+        
+        while True:
+        #obtain file payload
+        #payload = framedSock.framedReceive(sock, debug)
+            temp = sock.recv(1024)
+            thisBuffSize += len(temp)
+            temp = temp.decode()
+            payload += temp
+            if debug:
+                print('rec\'d: ', payload)
+            if not temp:
+                 break
+            if fileSize == thisBuffSize:
+                break
+    
+        with open(DIR + userInput, 'w') as wFile:
+            wFile.write(payload)
+                
+        print('Sucess, payload recieved from: ', userInput, '\n')
+        sock.sendall('done'.encode() )
+        # sys.exit(1)
+    elif userInput == 'exit':
+        print('exit by client.... goodbye')
+        sys.exit(0)
+    else:
+        print('From client: ',userInput, ' -> path doesnt exist, client needs to try again')
+        # sys.exit(1)
+        # continue
     
 
 
