c264cb1627c3f289a87b0392401a1bb9a50298db
jhidalgo4 <jhidalgo4@miners.utep.edu>
2020-10-19 21:49:50 -0600

    working threading connection

--- file-transfer-lab-process/fileClient.py
+++ file-transfer-lab-process/fileClient.py
@@ -1,14 +1,16 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thur Sep 24 19:02:15 2020
-@author: joaquin
-"""
-import socket, sys, re, os
-sys.path.append("../lib")
+# #!/usr/bin/env python3
+# # -*- coding: utf-8 -*-
+# """
+# Created on Thur Sep 24 19:02:15 2020
+# @author: joaquin
+# """
+
+import socket, sys, re
+
+sys.path.append("../lib")       # for params
 import params
-sys.path.append("../framed-echo")
-import framedSock
+
+from encapFramedSock import EncapFramedSock
 
 switchesVarDefaults = (
     (('-s', '--server'), 'server', "127.0.0.1:50001"),
@@ -27,9 +29,6 @@ if usage:
 try:
     serverHost, serverPort = re.split(":", server)
     serverPort = int(serverPort)
-    proxySetting = input('run with proxy? ( y / n ) :')
-    if proxySetting == 'y':
-        serverPort = int(50000)
 except:
     print("Can't parse server:port from '%s'" % server)
     sys.exit(1)
@@ -38,34 +37,108 @@ addrFamily = socket.AF_INET
 socktype = socket.SOCK_STREAM
 addrPort = (serverHost, serverPort)
 
-print('client\n')
+sock = socket.socket(addrFamily, socktype)
+
+if sock is None:
+    print('could not open socket')
+    sys.exit(1)
+
+sock.connect(addrPort)
+
+fsock = EncapFramedSock((sock, addrPort))
+
+###########
 while True:
+    #User Input
+    userInput = input('Enter here-->:')
     
-    s = socket.socket(addrFamily, socktype)
-    if s is None:
-        print('could not open socket')
-        sys.exit(1)
-    s.connect(addrPort)
-    
-    #input
-    cmmdLine = input('Enter (exit) or file name: ')
-    
-    #Handle exit
-    if cmmdLine == 'exit':
-        print('exit, goodbye')
-        framedSock.framedSend(s, bytes('exit', encoding='utf-8'), debug) # send file name or 1st command
+    if userInput == 'exit':
+        print('exit, goodbye...')
         sys.exit(0)
     
-    #Send file name
-    print('sending to server: ', cmmdLine)
-    framedSock.framedSend(s, bytes(cmmdLine, encoding='utf-8'), debug) # send file name or 1st command 
+    #Transfer
+    print(userInput)
+    fsock.send( bytes(userInput, encoding='utf-8'), debug)
+    
+    
+    #Status recieved
+    status = fsock.receive(debug)
+    print("received:", status )
 
 
-    #status
-    status = framedSock.framedReceive(s, debug).decode()
-    if status == 'exit':
-        sys.exit(0)
-    else:
-        print('\nStatus from server: ', status)
-        print('\n')
-        s.close()
+
+
+
+
+
+
+
+
+
+
+###############################3
+# import socket, sys, re, os
+# sys.path.append("../lib")
+# import params
+# sys.path.append("../framed-echo")
+# import framedSock
+
+# switchesVarDefaults = (
+#     (('-s', '--server'), 'server', "127.0.0.1:50001"),
+#     (('-d', '--debug'), "debug", False), # boolean (set if present)
+#     (('-?', '--usage'), "usage", False), # boolean (set if present)
+#     )
+
+# progname = "framedClient"
+# paramMap = params.parseParams(switchesVarDefaults)
+
+# server, usage, debug  = paramMap["server"], paramMap["usage"], paramMap["debug"]
+
+# if usage:
+#     params.usage()
+
+# try:
+#     serverHost, serverPort = re.split(":", server)
+#     serverPort = int(serverPort)
+#     proxySetting = input('run with proxy? ( y / n ) :')
+#     if proxySetting == 'y':
+#         serverPort = int(50000)
+# except:
+#     print("Can't parse server:port from '%s'" % server)
+#     sys.exit(1)
+
+# addrFamily = socket.AF_INET
+# socktype = socket.SOCK_STREAM
+# addrPort = (serverHost, serverPort)
+
+# print('client\n')
+# while True:
+    
+#     s = socket.socket(addrFamily, socktype)
+#     if s is None:
+#         print('could not open socket')
+#         sys.exit(1)
+#     s.connect(addrPort)
+    
+#     #input
+#     cmmdLine = input('Enter (exit) or file name: ')
+    
+#     #Handle exit
+#     if cmmdLine == 'exit':
+#         print('exit, goodbye')
+#         framedSock.framedSend(s, bytes('exit', encoding='utf-8'), debug) # send file name or 1st command
+#         sys.exit(0)
+    
+#     #Send file name
+#     print('sending to server: ', cmmdLine)
+#     framedSock.framedSend(s, bytes(cmmdLine, encoding='utf-8'), debug) # send file name or 1st command 
+
+
+#     #status
+#     status = framedSock.framedReceive(s, debug).decode()
+#     if status == 'exit':
+#         sys.exit(0)
+#     else:
+#         print('\nStatus from server: ', status)
+#         print('\n')
+#         s.close()
--- file-transfer-lab-process/fileServer.py
+++ file-transfer-lab-process/fileServer.py
@@ -1,17 +1,13 @@
-# #!/usr/bin/env python3
-# # -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+
+# -*- coding: utf-8 -*-
 """
 Created on Thur Sep 24 19:00:04 2020
 @author: joaquin
 """
-
-import socket, sys, re, os
-sys.path.append("../lib")
-import params
-sys.path.append("../framed-echo")
-import framedSock
-
-DIR = './recieve/'
+import sys
+sys.path.append("../lib")       # for params
+import re, socket, params, os
 
 switchesVarDefaults = (
     (('-l', '--listenPort') ,'listenPort', 50001),
@@ -31,48 +27,116 @@ lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # listener socket
 bindAddr = ("127.0.0.1", listenPort)
 lsock.bind(bindAddr)
 lsock.listen(5)
-print('Server\n')
 print("listening on:", bindAddr)
 
-while True:
-    sock, addr = lsock.accept()
+from threading import Thread;
+from encapFramedSock import EncapFramedSock
 
-    if not os.fork():
-        print("handling connection from: ", addr)
-        
-        print('waiting for input...\n')
-        cmdLine = framedSock.framedReceive(sock, debug) #
-        fileName = cmdLine.decode()
-        
-        if 'exit' in fileName:
-            print('exiting goodbye')
-            # framedSock.framedSend(sock, bytes('exit', encoding='utf-8'), debug) # send file name or 1st command 
-            print('\n')
-            sys.exit(0)
-        
-        else:
+class Server(Thread):
+    def __init__(self, sockAddr):
+        Thread.__init__(self)
+        self.sock, self.addr = sockAddr
+        self.fsock = EncapFramedSock(sockAddr)
+    def run(self):
+        print("new thread handling connection from", self.addr)
+        while True:
+            payload = self.fsock.receive(debug)
             
-            if debug: print("rec'd: ", cmdLine)
-            if not cmdLine:
-                if debug: print("child exiting")
-                sys.exit(0)
+            print('payL--->', payload)
+            #debug
+            if debug: print("rec'd: ", payload)
+            if not payload:     # done
+                if debug: print(f"thread connected to ((addr var goes here!))done")
+                # self.fsock.send(payload, debug)
+                self.fsock.close()
+                return          # exit
+            
+            status = payload
+            status += b"!"             # make emphatic!
+            self.fsock.send(status, debug)
+        
+
+while True:
+    sockAddr = lsock.accept()
+    server = Server(sockAddr)
+    server.start()
+
+
+
+
+
+
+
+
+#################################
+# import socket, sys, re, os
+# sys.path.append("../lib")
+# import params
+# sys.path.append("../framed-echo")
+# import framedSock
+
+# DIR = './recieve/'
+
+# switchesVarDefaults = (
+#     (('-l', '--listenPort') ,'listenPort', 50001),
+#     (('-d', '--debug'), "debug", False), # boolean (set if present)
+#     (('-?', '--usage'), "usage", False), # boolean (set if present)
+#     )
+
+# progname = "echoserver"
+# paramMap = params.parseParams(switchesVarDefaults)
+
+# debug, listenPort = paramMap['debug'], paramMap['listenPort']
+
+# if paramMap['usage']:
+#     params.usage()
+
+# lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # listener socket
+# bindAddr = ("127.0.0.1", listenPort)
+# lsock.bind(bindAddr)
+# lsock.listen(5)
+# print('Server\n')
+# print("listening on:", bindAddr)
+
+# while True:
+#     sock, addr = lsock.accept()
+
+#     if not os.fork():
+#         print("handling connection from: ", addr)
+        
+#         print('waiting for input...\n')
+#         cmdLine = framedSock.framedReceive(sock, debug) #
+#         fileName = cmdLine.decode()
+        
+#         if 'exit' in fileName:
+#             print('exiting goodbye')
+#             # framedSock.framedSend(sock, bytes('exit', encoding='utf-8'), debug) # send file name or 1st command 
+#             print('\n')
+#             sys.exit(0)
+        
+#         else:
+            
+#             if debug: print("rec'd: ", cmdLine)
+#             if not cmdLine:
+#                 if debug: print("child exiting")
+#                 sys.exit(0)
                 
-            #write payload
-            if os.path.exists(fileName):
-                print('obtained, writing to:', fileName )
-                payload = ''
+#             #write payload
+#             if os.path.exists(fileName):
+#                 print('obtained, writing to:', fileName )
+#                 payload = ''
                 
-                with open(fileName, 'rb') as fContext:
-                    payload = fContext.read()
+#                 with open(fileName, 'rb') as fContext:
+#                     payload = fContext.read()
                 
-                with open(DIR+fileName, 'wb') as wFile:
-                    wFile.write(payload)
+#                 with open(DIR+fileName, 'wb') as wFile:
+#                     wFile.write(payload)
                 
                 
-                print('Done writing to: ', cmdLine.decode() )
-                framedSock.framedSend(sock, bytes('DONE WRITING', encoding='utf-8'), debug) # send file name or 1st command 
-                print('\n')
+#                 print('Done writing to: ', cmdLine.decode() )
+#                 framedSock.framedSend(sock, bytes('DONE WRITING', encoding='utf-8'), debug) # send file name or 1st command 
+#                 print('\n')
                 
-            else:
-                print('ERROR: couldnt find file: ', fileName)
-                framedSock.framedSend(sock, bytes('ERROR \nFile doesnt exist', encoding='utf-8'), debug) #
+#             else:
+#                 print('ERROR: couldnt find file: ', fileName)
+#                 framedSock.framedSend(sock, bytes('ERROR \nFile doesnt exist', encoding='utf-8'), debug) #
