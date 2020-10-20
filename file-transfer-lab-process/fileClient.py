ea9a12e21d2aaa30854a0db80ac188a0dff75c91
jhidalgo4 <jhidalgo4@miners.utep.edu>
2020-10-19 22:34:34 -0600

    adding locking to threads and use same system from forking lab

--- file-transfer-lab-process/fileClient.py
+++ file-transfer-lab-process/fileClient.py
@@ -1,9 +1,9 @@
-# #!/usr/bin/env python3
-# # -*- coding: utf-8 -*-
-# """
-# Created on Thur Oct 02 12:01:25 2020
-# @author: joaquin
-# """
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thur Oct 02 12:01:25 2020
+@author: joaquin
+"""
 
 import socket, sys, re
 
@@ -64,79 +64,3 @@ while True:
     print("Status from Server: ", status , '\n')
 
 
-
-
-
-
-
-
-
-
-
-
-###############################3
-# import socket, sys, re, os
-# sys.path.append("../lib")
-# import params
-# sys.path.append("../framed-echo")
-# import framedSock
-
-# switchesVarDefaults = (
-#     (('-s', '--server'), 'server', "127.0.0.1:50001"),
-#     (('-d', '--debug'), "debug", False), # boolean (set if present)
-#     (('-?', '--usage'), "usage", False), # boolean (set if present)
-#     )
-
-# progname = "framedClient"
-# paramMap = params.parseParams(switchesVarDefaults)
-
-# server, usage, debug  = paramMap["server"], paramMap["usage"], paramMap["debug"]
-
-# if usage:
-#     params.usage()
-
-# try:
-#     serverHost, serverPort = re.split(":", server)
-#     serverPort = int(serverPort)
-#     proxySetting = input('run with proxy? ( y / n ) :')
-#     if proxySetting == 'y':
-#         serverPort = int(50000)
-# except:
-#     print("Can't parse server:port from '%s'" % server)
-#     sys.exit(1)
-
-# addrFamily = socket.AF_INET
-# socktype = socket.SOCK_STREAM
-# addrPort = (serverHost, serverPort)
-
-# print('client\n')
-# while True:
-    
-#     s = socket.socket(addrFamily, socktype)
-#     if s is None:
-#         print('could not open socket')
-#         sys.exit(1)
-#     s.connect(addrPort)
-    
-#     #input
-#     cmmdLine = input('Enter (exit) or file name: ')
-    
-#     #Handle exit
-#     if cmmdLine == 'exit':
-#         print('exit, goodbye')
-#         framedSock.framedSend(s, bytes('exit', encoding='utf-8'), debug) # send file name or 1st command
-#         sys.exit(0)
-    
-#     #Send file name
-#     print('sending to server: ', cmmdLine)
-#     framedSock.framedSend(s, bytes(cmmdLine, encoding='utf-8'), debug) # send file name or 1st command 
-
-
-#     #status
-#     status = framedSock.framedReceive(s, debug).decode()
-#     if status == 'exit':
-#         sys.exit(0)
-#     else:
-#         print('\nStatus from server: ', status)
-#         print('\n')
-#         s.close()
